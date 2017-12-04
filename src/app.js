cbe52119659d15ce17b3355d2889020a82fd930c
OBRIEN66 <nicholas.obrien@ucc.on.ca>
2017-12-04 14:01:58 -0500

    should work

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ var config = require("./pref")
 var server = http.createServer((req, res) => {
 	var styles = fs.readFileSync(__dirname + "/master.css", "utf8").replace(/\n|\t/g, "")
 	if (req.method === "GET") {
-		var url = req.url.replace(/%20/gi, " ")
-		var filePath = path.join(__dirname, "../" + config.content, url.replace(config.serverdir, ""))
+		var url = req.url.replace(/%20/gi, " ").replace(config.serverdir, "")
+		var filePath = path.join(__dirname, "../" + config.content, url)
 
 		if (filePath.split(".").reverse()[0] !== "pdf") {
 			fs.readdir(filePath, (err, files) => {
@@ -57,5 +57,5 @@ var server = http.createServer((req, res) => {
 		res.end("<h1>405: Method Not Allowed</h1>")
 	}
 })
-console.log("http://localhost:"+ config.port)
+console.log("http://localhost:"+ config.port + "/" + config.serverdir)
 server.listen(config.port)
